c11ecd8dfb03ec876726c7cc0d722a48d90792d2
MapleCCC <littlelittlemaple@gmail.com>
2021-09-30 00:53:16 +0800

    refactor: `tk_is_available()` uses more lightweight solution

--- recipes/sys.py
+++ recipes/sys.py
@@ -1,12 +1,10 @@
+from .importlib import importable
+
+
 __all__ = ["tk_is_available"]
 
 
 def tk_is_available() -> bool:
     """Check if Tk is available"""
 
-    try:
-        import tkinter  # type: ignore
-    except ModuleNotFoundError:
-        return False
-    else:
-        return True
+    return importable("tkinter")
