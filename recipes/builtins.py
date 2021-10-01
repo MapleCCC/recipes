fda33bfd85fc8e0387f8de5cf547718fad9492a4
MapleCCC <littlelittlemaple@gmail.com>
2021-10-01 19:27:57 +0800

    feat: add `read_text()` and `write_text()`

--- recipes/builtins.py
+++ recipes/builtins.py
@@ -1,9 +1,22 @@
+from __future__ import annotations  # for types imported from _typeshed
+
 import sys
 from collections.abc import MutableSequence
-from typing import Any, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar
 
 
-__all__ = ["hashable", "append", "eval_in_caller_frame", "NO_ARGUMENT"]
+if TYPE_CHECKING:
+    from _typeshed import StrPath
+
+
+__all__ = [
+    "hashable",
+    "append",
+    "eval_in_caller_frame",
+    "NO_ARGUMENT",
+    "read_text",
+    "write_text",
+]
 
 
 T = TypeVar("T")
@@ -36,3 +49,13 @@ def eval_in_caller_frame(expr: str):
 
 # A sentinel object to represent no argument to a parameter of a callable.
 NO_ARGUMENT: Any = object()
+
+
+def read_text(file: StrPath, encoding: str = "utf-8") -> str:
+    with open(file, "r", encoding=encoding) as f:
+        return f.read()
+
+
+def write_text(file: StrPath, text: str, encoding: str = "utf-8") -> None:
+    with open(file, "w", encoding=encoding) as f:
+        f.write(text)
