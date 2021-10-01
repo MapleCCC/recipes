a786c68b55a3ca825b1b2461a3bfa03ae9c43994
MapleCCC <littlelittlemaple@gmail.com>
2021-10-01 18:58:06 +0800

    refactor: use feature-rich libcst to replace the rough concrete syntax tree library in standard libraries

--- recipes/inspect.py
+++ recipes/inspect.py
@@ -1,10 +1,11 @@
 import inspect
-import parser
-import symbol
 from collections.abc import Callable
 from types import FrameType, FunctionType
 from typing import Any, Optional, Union
 
+import libcst as cst
+from libcst.helpers import ensure_type
+from more_itertools import one
 from typing_extensions import ParamSpec
 
 from .sourcelib import unindent_source
@@ -14,10 +15,6 @@ __all__ = ["get_function_body_source", "bind_arguments", "get_frame_curr_line"]
 
 
 P = ParamSpec("P")
-
-
-# FIXME It is quite insane to use a sequence of magic numbers to inspect syntax tree.
-#     Let's use an object-oriented concrete syntax tree library instead.
 
 
 def get_function_body_source(func: Union[str, FunctionType], unindent: bool = False) -> str:
@@ -30,37 +27,12 @@ def get_function_body_source(func: Union[str, FunctionType], unindent: bool = Fa
     Set the `unindent` parameter to `True` to return an unindented source code.
     """
 
-    # 1. Retrieve source code of the function, which is then parsed into concrete syntax
-    # tree.
-    #
-    # 2. Inspect the concrete syntax tree, and locate the position of the colon token
-    # which marks the delimitation between function header and function body.
-    #
-    # 3. Extract the source code of function body after the source line of the colon
-    # token.
-
     source = func if isinstance(func, str) else inspect.getsource(func)
+    source = unindent_source(source)
 
-    syntax_tree = parser.suite(unindent_source(source))
-
-    # Convert syntax tree to tuple representation
-    st_tuple = syntax_tree.totuple(line_info=True)
-
-    # Locate the colon token that marks the delimitations between function header and
-    # function body.
-    #
-    # Node chain: file_input -> stmt -> compound_stmt -> (decorated ->) funcdef -> COLON
-    compound_stmt = st_tuple[1][1]
-    if compound_stmt[1][0] == symbol.decorated:
-        funcdef = compound_stmt[1][2]
-    else:
-        funcdef = compound_stmt[1]
-    colon = funcdef[4]
-    colon_lineno = colon[2]
-
-    body_start_lineno = colon_lineno + 1
-    body_source_lines = source.splitlines()[body_start_lineno - 1 :]
-    body_source = "\n".join(body_source_lines)
+    module = cst.parse_module(source)
+    funcdef = ensure_type(one(module.body), cst.FunctionDef)
+    body_source = module.code_for_node(funcdef.body)
 
     return unindent_source(body_source) if unindent else body_source
 
