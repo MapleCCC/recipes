143b9ec29c5709bc5511ea82e6c0334924e0264d
MapleCCC <littlelittlemaple@gmail.com>
2021-10-05 01:18:22 +0800

    feat: add `ensure_type()`

--- recipes/builtins.py
+++ recipes/builtins.py
@@ -2,7 +2,9 @@ from __future__ import annotations  # for types imported from _typeshed
 
 import sys
 from collections.abc import MutableSequence
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar, Union, cast, overload
+
+import inflect
 
 
 if TYPE_CHECKING:
@@ -20,6 +22,12 @@ __all__ = [
 
 
 T = TypeVar("T")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
+
+
+p = inflect.engine()
 
 
 def hashable(obj: object, /) -> bool:
@@ -77,3 +85,58 @@ def write_text(file: StrPath, text: str, encoding: str = "utf-8") -> None:
 
     with open(file, "w", encoding=encoding) as f:
         f.write(text)
+
+
+@overload
+def ensure_type(obj: object, typ: type[T]) -> T:
+    ...
+
+
+@overload
+def ensure_type(obj: object, typ: tuple[type[T]]) -> T:
+    ...
+
+
+@overload
+def ensure_type(obj: object, typ: tuple[type[T], type[T2]]) -> Union[T, T2]:
+    ...
+
+
+@overload
+def ensure_type(
+    obj: object, typ: tuple[type[T], type[T2], type[T3]]
+) -> Union[T, T2, T3]:
+    ...
+
+
+@overload
+def ensure_type(
+    obj: object, typ: tuple[type[T], type[T2], type[T3], type[T4]]
+) -> Union[T, T2, T3, T4]:
+    ...
+
+
+@overload
+def ensure_type(obj: object, typ: tuple[type[T], ...]) -> T:
+    ...
+
+
+def ensure_type(obj: object, typ: Union[type[T], tuple[type[T], ...]]) -> T:
+    """
+    Return the same object unchanged, but raise TypeError if the object is not of the
+    specified type.
+
+    The `typ` parameter accepts either a type or a tuple of types.
+    """
+
+    if isinstance(obj, typ):
+        return cast(T, obj)
+
+    else:
+        types = typ if isinstance(typ, tuple) else (typ,)
+        type_names = [t.__name__ for t in types]
+        expected = p.a(p.join(type_names, conj="or"))
+
+        got = p.a(type(obj).__name__)
+
+        raise TypeError(f"expect {expected}, but got {got} instead")
