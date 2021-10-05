469d9c57a5f4778e50f87bdc9598844c21e11e6a
MapleCCC <littlelittlemaple@gmail.com>
2021-10-05 23:29:34 +0800

    fix: the string considered line boundary can contain more than one characters

--- recipes/string.py
+++ recipes/string.py
@@ -1,6 +1,22 @@
-__all__ = ["line_boundary"]
+__all__ = ["line_boundaries"]
 
 
-# A string containing characters considered line boundaries
+# A collection of strings considered line boundaries
 # Reference: https://docs.python.org/3.9/library/stdtypes.html#str.splitlines
-line_boundary = "\n\r\v\x0b\f\x0c\x1c\x1d\x1e\x85\u2028\u2029"
+line_boundaries = frozenset(
+    [
+        "\n",
+        "\r",
+        "\r\n",
+        "\v",
+        "\x0b",
+        "\f",
+        "\x0c",
+        "\x1c",
+        "\x1d",
+        "\x1e",
+        "\x85",
+        "\u2028",
+        "\u2029",
+    ]
+)
