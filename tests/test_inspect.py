b9b077484858c79c92f26d1bf27a00f9fc23cc68
MapleCCC <littlelittlemaple@gmail.com>
2021-10-06 02:51:43 +0800

    test: update unit tests for `get_function_body_source()`

--- tests/test_inspect.py
+++ tests/test_inspect.py
@@ -1,4 +1,9 @@
+import re
+
+import pytest
+
 from recipes.inspect import get_function_body_source
+from recipes.exceptions import OutdentedCommentError
 
 
 class TestGetFunctionBodySource:
@@ -7,7 +12,7 @@ class TestGetFunctionBodySource:
             a = 1
             b = 2
 
-        assert get_function_body_source(f) == "\n    a = 1\n    b = 2\n"
+        assert get_function_body_source(f) == "a = 1\nb = 2\n"
 
     def test_lambda(self) -> None:
         assert get_function_body_source(lambda: 321) == "321"
@@ -18,5 +23,22 @@ class TestGetFunctionBodySource:
                 a = 1
                 b = 2
 
-        assert get_function_body_source(A.b) == "\n    a = 1\n    b = 2\n"
-        assert get_function_body_source(A().b) == "\n    a = 1\n    b = 2\n"
+        assert get_function_body_source(A.b) == "a = 1\nb = 2\n"
+        assert get_function_body_source(A().b) == "a = 1\nb = 2\n"
+
+    # fmt: off
+    # temporarily disable black formatter, so that we can test the case with outdented comments.
+    def test_function_with_outdented_comments(self) -> None:
+        def f():
+            a = 1
+          # foo bar
+            b = 2
+
+        with pytest.raises(
+            OutdentedCommentError,
+            match=re.escape(
+                "get_function_body_source() expects no outdented comments in the body of the function"
+            ),
+        ):
+            get_function_body_source(f)
+    # fmt: on
