6e481262d48de83a8ccf6887349e61bca035739a
MapleCCC <littlelittlemaple@gmail.com>
2021-10-06 02:36:49 +0800

    test: add unit test to test `literal_block()` when the block of source contains outdented comments

--- tests/test_contextlib.py
+++ tests/test_contextlib.py
@@ -1,4 +1,9 @@
-from recipes.contextlib import skip_context, literal_block
+import re
+
+import pytest
+
+from recipes.contextlib import literal_block, skip_context
+from recipes.exceptions import OutdentedCommentError
 
 
 def test_skip_context() -> None:
@@ -9,10 +14,30 @@ def test_skip_context() -> None:
     assert a == 0
 
 
-def test_literal_block() -> None:
+class TestLiteralBlock:
+    """Unit tests for `literal_block()`"""
 
-    with literal_block() as source:
-        a = 1
-        b = 2
+    def test_normal_case(self) -> None:
 
-    assert source == "a = 1\nb = 2"
+        with literal_block() as source:
+            a = 1
+            b = 2
+
+        assert source == "a = 1\nb = 2\n"
+
+    # fmt: off
+    # temporarily disable black formatter, so that we can test the case with outdented comments.
+    def test_wiht_outdented_comment(self) -> None:
+
+        with pytest.raises(
+            OutdentedCommentError,
+            match=re.escape(
+                "the block in the body of literal_block() should not contain outdented comments"
+            ),
+        ):
+
+            with literal_block() as source:
+                a = 1
+              # foo bar
+                b = 2
+    # fmt: on
