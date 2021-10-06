0020b59f78c5fc0b1439edf0b6318481e3a870a4
MapleCCC <littlelittlemaple@gmail.com>
2021-10-07 06:35:55 +0800

    test: add unit tests for `literal_block()` error cases handling

--- tests/test_contextlib.py
+++ tests/test_contextlib.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 import pytest
 
@@ -51,6 +52,26 @@ class TestLiteralBlock:
             b = d
 
         assert source == "a = 1\nb = 4\n"
+
+    def test_invalid_argument(self) -> None:
+
+        with pytest.raises(ValueError, match="expect a user-defined function"):
+            literal_block(len)
+
+        with pytest.raises(ValueError, match="expect a user-defined function"):
+            literal_block(sys.exit)
+
+        with pytest.raises(ValueError, match="expect a user-defined function"):
+            literal_block(TestLiteralBlock)
+
+    def test_decorator_with_no_replacement_found(self) -> None:
+
+        with pytest.raises(NameError, match="has no replacement found"):
+
+            @literal_block
+            def source(c):
+                a = 1
+                b = c
 
     # fmt: off
     # temporarily disable black formatter, so that we can test the case with outdented comments.
