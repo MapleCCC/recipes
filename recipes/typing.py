d1ad5d693c6b389e344c10c2ba6eb22d0543e65e
MapleCCC <littlelittlemaple@gmail.com>
2021-11-05 20:49:21 +0800

    feat: add `Eq` and `Ord` from https://github.com/MapleCCC/Cito

--- recipes/typing.py
+++ recipes/typing.py
@@ -1,10 +1,10 @@
 from collections.abc import Callable
-from typing import TypeVar
+from typing import Protocol, TypeVar
 
 from typing_extensions import ParamSpec
 
 
-__all__ = ["IdentityDecorator"]
+__all__ = ["IdentityDecorator", "Eq", "Ord"]
 
 
 P = ParamSpec("P")
@@ -15,3 +15,25 @@ R = TypeVar("R")
 class IdentityDecorator:
     def __call__(self, __func: Callable[P, R]) -> Callable[P, R]:
         ...
+
+
+class Eq(Protocol):
+    def __eq__(self, __other) -> bool:
+        return self == __other
+
+    def __ne__(self, __other) -> bool:
+        return not self.__eq__(__other)
+
+
+class Ord(Eq, Protocol):
+    def __lt__(self, __other) -> bool:
+        return self < __other
+
+    def __le__(self, __other) -> bool:
+        return self.__lt__(__other) or self.__eq__(__other)
+
+    def __gt__(self, __other) -> bool:
+        return not self.__le__(__other)
+
+    def __ge__(self, __other) -> bool:
+        return not self.__lt__(__other)
