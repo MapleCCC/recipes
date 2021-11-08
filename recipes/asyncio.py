34c48ff4ba7247414237f5f9b5c934079622f82c
MapleCCC <littlelittlemaple@gmail.com>
2021-11-08 23:47:12 +0800

    feat: add recipes `aread_text()` and `awrite_text()`

--- recipes/asyncio.py
+++ recipes/asyncio.py
@@ -5,12 +5,14 @@ from collections.abc import Sequence
 from subprocess import CalledProcessError
 from typing import TYPE_CHECKING
 
+from .builtins import read_text, write_text
+
 
 if TYPE_CHECKING:
-    from _typeshed import StrOrBytesPath
+    from _typeshed import StrOrBytesPath, StrPath
 
 
-__all__ = ["asyncio_subprocess_check_output"]
+__all__ = ["asyncio_subprocess_check_output", "aread_text", "awrite_text"]
 
 
 async def asyncio_subprocess_check_output(
@@ -32,3 +34,29 @@ async def asyncio_subprocess_check_output(
         raise CalledProcessError(retcode, args, stdout, stderr)
 
     return stdout
+
+
+async def aread_text(file: StrPath, encoding: str = "utf-8") -> str:
+    """
+    Asynchronously read text in UTF-8 encoding.
+
+    This is mostly a convenient wrapper to reduce the boilerplate of `open()` and
+    `Path.read_text()`. The caller doesn't need to specify the `"r"` and `"t"` flags and
+    doesn't need to explicitly specify `encoding="utf-8"` in platforms where
+    `locale.getpreferredencoding()` doesn't return "UTF-8".
+    """
+
+    return await asyncio.to_thread(read_text, file, encoding)
+
+
+async def awrite_text(file: StrPath, text: str, encoding: str = "utf-8") -> None:
+    """
+    Asynchronously write text in UTF-8 encoding.
+
+    This is mostly a convenient wrapper to reduce the boilerplate of `open()` and
+    `Path.write_text()`. The caller doesn't need to specify the `"w"` and `"t"` flags
+    and doesn't need to explicitly specify `encoding="utf-8"` in platforms where
+    `locale.getpreferredencoding()` doesn't return "UTF-8".
+    """
+
+    return await asyncio.to_thread(write_text, file, text, encoding)
