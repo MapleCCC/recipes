3e5448c87e5d2dde7e7a15ed3ff058485c401638
MapleCCC <littlelittlemaple@gmail.com>
2022-07-25 17:14:57 +0800

    update: typing.py

--- recipes/typing.py
+++ recipes/typing.py
@@ -53,63 +53,33 @@ class Ord(Eq, Protocol):
         return not self.__lt__(__other)
 
 
+# fmt: off
 class P0K0Callable(Protocol[R_co]):
-    def __call__(self) -> R_co:
-        ...
-
-
+    def __call__(self) -> R_co: ...
 class P0KNCallable(Protocol[T_contra, R_co]):
-    def __call__(self, **__kwarg: T_contra) -> R_co:
-        ...
-
-
+    def __call__(self, **__kwarg: T_contra) -> R_co: ...
 class PNK0Callable(Protocol[T_contra, R_co]):
-    def __call__(self, *__args: T_contra) -> R_co:
-        ...
-
-
+    def __call__(self, *__args: T_contra) -> R_co: ...
 class PNKNCallable(Protocol[T_contra, S_contra, R_co]):
-    def __call__(self, *__args: T_contra, **__kwargs: S_contra) -> R_co:
-        ...
+    def __call__(self, *__args: T_contra, **__kwargs: S_contra) -> R_co: ...
+# fmt: on
 
 
 # TODO can we use @overload to unify these classes into one ?
 
 
+# fmt: off
 class P1K0Callable(Protocol[T_contra, R_co]):
-    def __call__(self, __arg: T_contra) -> R_co:
-        ...
-
-
+    def __call__(self, __arg: T_contra) -> R_co: ...
 class P2K0Callable(Protocol[T_contra, T1_contra, R_co]):
-    def __call__(self, __x: T_contra, __x1: T1_contra) -> R_co:
-        ...
-
-
+    def __call__(self, __x: T_contra, __x1: T1_contra) -> R_co: ...
 class P3K0Callable(Protocol[T_contra, T1_contra, T2_contra, R_co]):
-    def __call__(self, __x: T_contra, __x1: T1_contra, __x2: T2_contra) -> R_co:
-        ...
-
-
+    def __call__(self, __x: T_contra, __x1: T1_contra, __x2: T2_contra) -> R_co: ...
 class P4K0Callable(Protocol[T_contra, T1_contra, T2_contra, T3_contra, R_co]):
-    def __call__(
-        self, __x: T_contra, __x1: T1_contra, __x2: T2_contra, __x3: T3_contra
-    ) -> R_co:
-        ...
-
-
-class P5K0Callable(
-    Protocol[T_contra, T1_contra, T2_contra, T3_contra, T4_contra, R_co]
-):
-    def __call__(
-        self,
-        __x: T_contra,
-        __x1: T1_contra,
-        __x2: T2_contra,
-        __x3: T3_contra,
-        __x4: T4_contra,
-    ) -> R_co:
-        ...
+    def __call__(self, __x: T_contra, __x1: T1_contra, __x2: T2_contra, __x3: T3_contra) -> R_co: ...
+class P5K0Callable(Protocol[T_contra, T1_contra, T2_contra, T3_contra, T4_contra, R_co]):
+    def __call__(self, __x: T_contra, __x1: T1_contra, __x2: T2_contra, __x3: T3_contra, __x4: T4_contra,) -> R_co: ...
+# fmt: on
 
 
 K0Callable = (
@@ -123,55 +93,20 @@ K0Callable = (
 )
 
 
+# fmt: off
 class P1K1Callable(Protocol[T_contra, S_contra, R_co]):
-    def __call__(self, __arg: T_contra, *, __x: S_contra) -> R_co:
-        ...
-
-
+    def __call__(self, __arg: T_contra, *, __x: S_contra) -> R_co: ...
 class P1K2Callable(Protocol[T_contra, S_contra, S1_contra, R_co]):
-    def __call__(self, __arg: T_contra, *, __x: S_contra, __x1: S1_contra) -> R_co:
-        ...
-
-
+    def __call__(self, __arg: T_contra, *, __x: S_contra, __x1: S1_contra) -> R_co: ...
 class P1K3Callable(Protocol[T_contra, S_contra, S1_contra, S2_contra, R_co]):
-    def __call__(
-        self, __arg: T_contra, *, __x: S_contra, __x1: S1_contra, __x2: S2_contra
-    ) -> R_co:
-        ...
-
-
+    def __call__(self, __arg: T_contra, *, __x: S_contra, __x1: S1_contra, __x2: S2_contra) -> R_co: ...
 class P1K4Callable(Protocol[T_contra, S_contra, S1_contra, S2_contra, S3_contra, R_co]):
-    def __call__(
-        self,
-        __arg: T_contra,
-        *,
-        __x: S_contra,
-        __x1: S1_contra,
-        __x2: S2_contra,
-        __x3: S3_contra
-    ) -> R_co:
-        ...
-
-
-class P1K5Callable(
-    Protocol[T_contra, S_contra, S1_contra, S2_contra, S3_contra, S4_contra, R_co]
-):
-    def __call__(
-        self,
-        __arg: T_contra,
-        *,
-        __x: S_contra,
-        __x1: S1_contra,
-        __x2: S2_contra,
-        __x3: S3_contra,
-        __x4: S4_contra
-    ) -> R_co:
-        ...
-
-
+    def __call__(self, __arg: T_contra, *, __x: S_contra, __x1: S1_contra, __x2: S2_contra, __x3: S3_contra) -> R_co: ...
+class P1K5Callable(Protocol[T_contra, S_contra, S1_contra, S2_contra, S3_contra, S4_contra, R_co]):
+    def __call__(self, __arg: T_contra, *, __x: S_contra, __x1: S1_contra, __x2: S2_contra, __x3: S3_contra, __x4: S4_contra) -> R_co: ...
 class P1KNCallable(Protocol[T_contra, S_contra, R_co]):
-    def __call__(self, __arg: T_contra, **__kwargs: S_contra) -> R_co:
-        ...
+    def __call__(self, __arg: T_contra, **__kwargs: S_contra) -> R_co: ...
+# fmt: on
 
 
 P1Callable = (
@@ -185,48 +120,18 @@ P1Callable = (
 )
 
 
+# fmt: off
 class PNK1Callable(Protocol[T_contra, S_contra, R_co]):
-    def __call__(self, *__args: T_contra, __x: S_contra) -> R_co:
-        ...
-
-
+    def __call__(self, *__args: T_contra, __x: S_contra) -> R_co: ...
 class PNK2Callable(Protocol[T_contra, S_contra, S1_contra, R_co]):
-    def __call__(self, *__args: T_contra, __x: S_contra, __x1: S1_contra) -> R_co:
-        ...
-
-
+    def __call__(self, *__args: T_contra, __x: S_contra, __x1: S1_contra) -> R_co: ...
 class PNK3Callable(Protocol[T_contra, S_contra, S1_contra, S2_contra, R_co]):
-    def __call__(
-        self, *__args: T_contra, __x: S_contra, __x1: S1_contra, __x2: S2_contra
-    ) -> R_co:
-        ...
-
-
+    def __call__(self, *__args: T_contra, __x: S_contra, __x1: S1_contra, __x2: S2_contra) -> R_co: ...
 class PNK4Callable(Protocol[T_contra, S_contra, S1_contra, S2_contra, S3_contra, R_co]):
-    def __call__(
-        self,
-        *__args: T_contra,
-        __x: S_contra,
-        __x1: S1_contra,
-        __x2: S2_contra,
-        __x3: S3_contra
-    ) -> R_co:
-        ...
-
-
-class PNK5Callable(
-    Protocol[T_contra, S_contra, S1_contra, S2_contra, S3_contra, S4_contra, R_co]
-):
-    def __call__(
-        self,
-        *__args: T_contra,
-        __x: S_contra,
-        __x1: S1_contra,
-        __x2: S2_contra,
-        __x3: S3_contra,
-        __x4: S4_contra
-    ) -> R_co:
-        ...
+    def __call__(self, *__args: T_contra, __x: S_contra, __x1: S1_contra, __x2: S2_contra, __x3: S3_contra) -> R_co: ...
+class PNK5Callable(Protocol[T_contra, S_contra, S1_contra, S2_contra, S3_contra, S4_contra, R_co]):
+    def __call__(self, *__args: T_contra, __x: S_contra, __x1: S1_contra, __x2: S2_contra, __x3: S3_contra, __x4: S4_contra) -> R_co: ...
+# fmt: on
 
 
 PNCallable = (
@@ -238,3 +143,19 @@ PNCallable = (
     | PNK1Callable[T, S, R]
     | PNK0Callable[T, R]
 )
+
+
+# TODO programmatically generate instead of hand-written
+#
+# PKCallable = (
+#     PNCallable[T, S, R]
+#     | P5Callable[T, S, R]
+#     | P4Callable[T, S, R]
+#     | P3Callable[T, S, R]
+#     | P2Callable[T, S, R]
+#     | P1Callable[T, S, R]
+#     | P0Callable[S, R]
+# )
+
+# TODO move to `typing.callables` module
+# TODO maybe make it a standalone library ?
