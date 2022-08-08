e9373cdf574c8d6337dda30aa43fbceabc4663bb
MapleCCC <littlelittlemaple@gmail.com>
2022-08-09 01:03:34 +0800

    fix: turn out that Unreachable is still useful when type checker fails to prove unreachability

--- recipes/exceptions.py
+++ recipes/exceptions.py
@@ -1,9 +1,21 @@
 __all__ = ["Unreachable", "OutdentedCommentError"]
 
 
-# Supplanted by `typing.assert_never()`
+# XXX whether to inherit from `RuntimeError` or `Exception` ?
 class Unreachable(RuntimeError):
-    """Raised when supposedly unreachable code is reached"""
+    """
+    Raised when supposedly unreachable code is reached
+
+    Some branch is theoretically unreachable, but surprisingly jumped in.
+
+    Alternative to `raise Unreachable` is to either `raise`ValueError` or `typing.assert_never()`.
+
+    1. When the unreachability is up to a user-supplied argument, choose `raise ValueError`.
+    2. When the unreachability is provable by type checker, choose `typing.assert_never()`.
+    3. When the unreachability is unprovable by type checker, choose `raise Unreachable`.
+
+    Reference: https://typing.readthedocs.io/en/latest/source/unreachable.html
+    """
 
 
 class OutdentedCommentError(Exception):
